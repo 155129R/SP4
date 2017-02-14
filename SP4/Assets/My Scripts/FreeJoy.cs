4dbf8db398fac4f5c769486eda7685b0a76a33b5
BuildTools <unconfigured@null.spigotmc.org>
2017-02-14 14:43:55 +0800

    First framework update: - Multiplayer(Zenden) - Movement(Zenden)

---
+++ SP4/Assets/My Scripts/FreeJoy.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class FreeJoy : MonoBehaviour{
+
+    public UnityEvent SwipeUp;
+    public UnityEvent SwipeDown;
+    public UnityEvent SwipeLeft;
+    public UnityEvent SwipeRight;
+    public float swipeSensitivity = 0.2f;
+
+    public Image joyBG;
+    public Image joyThumb;
+    public float maxLength = 5.0f;
+
+    private float timerForSwipe = 0.0f;
+    private Vector3 dir;
+    private bool active;
+
+	// Use this for initialization
+	void Start () {
+        timerForSwipe = 0.0f;
+        active = false;
+        joyBG.enabled =  false;
+        joyThumb.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(active)
+        {
+            if (timerForSwipe <= swipeSensitivity)
+                timerForSwipe += Time.deltaTime;
+
+            dir = Input.mousePosition - joyBG.rectTransform.position;
+            float analogLength = dir.magnitude;
+
+            if (analogLength > maxLength)
+                analogLength = maxLength;
+
+            joyThumb.rectTransform.localPosition = dir.normalized * analogLength;
+        }
+        else
+        {
+            dir.Set(0, 0, 0);
+        }
+	}
+
+    public Vector3 GetInput()
+    {
+        return dir.normalized;
+    }
+
+    public void OnTouch()
+    {
+        active = true;
+        joyBG.rectTransform.position = Input.mousePosition;
+        joyBG.enabled = true;
+        joyThumb.enabled = true;
+        timerForSwipe = 0.0f;
+    }
+
+    public void OnRelease()
+    {
+        active = false;
+        joyBG.enabled = false;
+        joyThumb.enabled = false;
+
+        if (timerForSwipe <= swipeSensitivity && dir.sqrMagnitude != 0)
+        {
+            if(Mathf.Abs(dir.y) > Mathf.Abs(dir.x))
+            {//vertical swipe
+                if (dir.y > 0)
+                    SwipeUp.Invoke();
+                else
+                    SwipeDown.Invoke();
+            }
+            else
+            {//horizontal swipe
+                if (dir.x > 0)
+                    SwipeRight.Invoke();
+                else
+                    SwipeLeft.Invoke();
+            }
+        }
+    }
+}
---
+++ SP4/Assets/My Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Movement : NetworkBehaviour {
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * 3;
+
+        myTransform.Translate(x, y  , 0);
+	}
+}
